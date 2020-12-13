59782bea4e7b29ad43d0fa5dd494cb476396e392
Bryson Palmer <brysonpalmer@gmail.com>
2020-12-13 10:38:13 -0800

    Added a basic framework for the app.js file. Made templates for the manager, employee type, engineer, and intern functions.

--- app.js
+++ app.js
@@ -10,6 +10,126 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamMembers = [];
+
+// Ask user for Manager info
+function askUserForManagerInfo() {
+
+    return inquirer.prompt([
+
+        {
+            message: "Ask a question",
+            name: "replaceMe",
+            type: "input"
+        }
+
+    ]).then( ( managerData ) => {
+
+        // Create new instance of a Manager
+        const newManager = new Manager ( managerData.name, managerData.id, managerData.email, managerData.officeNumber );
+
+        teamMembers.push ( newManager );
+
+        askUserForEmployeeType();
+
+    });
+
+}
+
+// Ask user for next employee type
+function askUserForEmployeeType() {
+
+    return inquirer.prompt([
+
+        // List type prompt
+        {
+            message: "Ask a question",
+            name: "replaceMe",
+            type: "input"
+        }
+
+    ]).then( ( newEmployeeChoiceData ) => {
+
+        // Conditional
+        // IF they selected a new Engineer
+        askUserForEngineerInfo();
+
+        // ELSE if the user selected a new Intern
+        askUserForInternInfo();
+
+        // ELSE create the output
+        createHtmlFile();
+
+        
+    });
+
+}
+
+// Ask user for Engineer info
+function askUserForEngineerInfo() {
+
+    return inquirer.prompt([
+
+        {
+            message: "Ask a question",
+            name: "replaceMe",
+            type: "input"
+        }
+
+    ]).then( (EngineerData) => {
+
+        // Create new instance of a Manager
+        const newEngineer = new Engineer ( EngineerData.name, EngineerData.id, EngineerData.email, EngineerData.github );
+
+        teamMembers.push ( newEngineer );
+
+        askUserForEmployeeType();
+
+    });
+
+}
+
+// Ask user for Intern info
+function askUserForInternInfo() {
+
+    return inquirer.prompt([
+
+        {
+            message: "Ask a question",
+            name: "replaceMe",
+            type: "input"
+        }
+
+    ]).then( (InternData) => {
+
+        // Create new instance of a Manager
+        const newIntern = new Intern ( InternData.name, InternData.id, InternData.email, InternData.school );
+
+        teamMembers.push ( newIntern );
+
+        askUserForEmployeeType();
+
+    });
+
+}
+
+// Create HTML file
+function createHtmlFile() {
+
+    const htmlContent = render( teamMembers );
+
+    // Use the FS module to create the output file
+
+
+};
+
+askUserForManagerInfo();
+
+// Make it work then make it modular
+
+// Add console.log("\n"); line breaks
+
+// Add color headers. Dif color for each employee type
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
