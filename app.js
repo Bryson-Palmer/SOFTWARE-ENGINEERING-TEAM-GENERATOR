cb68bf39722b19c28c4e84a4f4335abfedbbeb27
Bryson Palmer <brysonpalmer@gmail.com>
2020-12-14 00:04:23 -0800

    Added the functionality for the Manager funciton, askUserForEmployeeType function, and the engineer. Also, added functionality to the createHtmlFile function.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const chalk = require("chalk");
 const path = require("path");
+const util = require("util");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -10,26 +12,69 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
 const teamMembers = [];
+
 
 // Ask user for Manager info
 function askUserForManagerInfo() {
 
+    console.log( chalk.blueBright("\n\n------------------------------------------------\n") );
+    console.log( chalk.greenBright("Welcome to the Software Engineer Team Generator.\n") );
+    console.log( chalk.greenBright("-------- Lets start building your team! --------\n") );
+    console.log( chalk.greenBright("             *          *          *               ") );
+    console.log( chalk.greenBright("            * *        * *        * *              ") );
+    console.log( chalk.greenBright("           ** **      ** **      ** **             ") );
+    console.log( chalk.blueBright("\n---- To begin, add your manager information.----\n") );
+    console.log( chalk.blueBright("------------------------------------------------\n\n") );
+
+
     return inquirer.prompt([
 
         {
-            message: "Ask a question",
-            name: "replaceMe",
-            type: "input"
-        }
+            type: "input",
+            name: "managerName",
+            message: "Enter your name: ",
+            validate: function validateName( managerName ) {
+                return managerName !== "";
+            }
+        },
 
-    ]).then( ( managerData ) => {
+        {
+            type: "number",
+            name: "idNumber",
+            message: "Enter your ID number: ",
+            validate: function validateID( idNumber ) {
+                return idNumber !== "";
+            }
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email: ",
+            validate: function validateEmail( email ) {
+                return email !== "";
+            }
+        },
+
+        {
+            type: "number",
+            name: "officeNumber",
+            message: "Enter your office number: ",
+            validate: function validateID( officeNumber ) {
+                return officeNumber !== "";
+            }
+        },
+
+    ]).then( (managerData) => {
 
         // Create new instance of a Manager
-        const newManager = new Manager ( managerData.name, managerData.id, managerData.email, managerData.officeNumber );
+        const newManager = new Manager ( managerData.managerName, managerData.idNumber, managerData.email, managerData.officeNumber );
 
-        teamMembers.push ( newManager );
-
+        teamMembers.push( newManager );
+        
         askUserForEmployeeType();
 
     });
@@ -39,27 +84,35 @@ function askUserForManagerInfo() {
 // Ask user for next employee type
 function askUserForEmployeeType() {
 
+    console.log( chalk.blueBright("\n\n------------------------------------------------\n\n") );
+
+
     return inquirer.prompt([
 
         // List type prompt
         {
-            message: "Ask a question",
-            name: "replaceMe",
-            type: "input"
+            type: "list",
+            name: "employeeType",
+            message: "Do you want to add another team member? \n\nPlease choose from the following options.",
+            choices: ["Add an Engineer", "Add an Intern", "I'm all done adding team members"],
+            
         }
 
     ]).then( ( newEmployeeChoiceData ) => {
 
+
         // Conditional
-        // IF they selected a new Engineer
-        askUserForEngineerInfo();
+        if( newEmployeeChoiceData.employeeType == "Add an Engineer" ) {
+            // IF they selected a new Engineer
+            askUserForEngineerInfo();
+        
+        } else if( newEmployeeChoiceData.employeeType == "Add an Intern" ) {
+            // ELSE if the user selected a new Intern
+            askUserForInternInfo();
 
-        // ELSE if the user selected a new Intern
-        askUserForInternInfo();
-
-        // ELSE create the output
-        createHtmlFile();
-
+        } else
+            // ELSE create the output
+            createHtmlFile();
         
     });
 
@@ -68,18 +121,52 @@ function askUserForEmployeeType() {
 // Ask user for Engineer info
 function askUserForEngineerInfo() {
 
+    console.log( chalk.blueBright("\n\n\n---------- Now, lets add an Engineer. ---------\n") );
+    console.log( chalk.blueBright("------------------------------------------------\n\n") );
+
+
     return inquirer.prompt([
 
         {
-            message: "Ask a question",
-            name: "replaceMe",
-            type: "input"
-        }
+            type: "input",
+            name: "engineerName",
+            message: "Enter Engineer's name: ",
+            validate: function validateName( engineerName ) {
+                return engineerName !== "";
+            }
+        },
+
+        {
+            type: "number",
+            name: "idNumber",
+            message: "Enter thier ID number: ",
+            validate: function validateID( idNumber ) {
+                return idNumber !== "";
+            }
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "Enter their email: ",
+            validate: function validateEmail( email ) {
+                return email !== "";
+            }
+        },
+
+        {
+            type: "number",
+            name: "github",
+            message: "Enter their Git Hub account: ",
+            validate: function validateID( github ) {
+                return github !== "";
+            }
+        },
 
     ]).then( (EngineerData) => {
 
         // Create new instance of a Manager
-        const newEngineer = new Engineer ( EngineerData.name, EngineerData.id, EngineerData.email, EngineerData.github );
+        const newEngineer = new Engineer ( EngineerData.engineerName, EngineerData.idNumber, EngineerData.email, EngineerData.github );
 
         teamMembers.push ( newEngineer );
 
@@ -119,7 +206,8 @@ function createHtmlFile() {
     const htmlContent = render( teamMembers );
 
     // Use the FS module to create the output file
-
+    writeFileAsync(outputPath, htmlContent);
+    
 
 };
 
@@ -129,7 +217,7 @@ askUserForManagerInfo();
 
 // Add console.log("\n"); line breaks
 
-// Add color headers. Dif color for each employee type
+// Add color headers. Dif color for each employee type. Use chalk
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
