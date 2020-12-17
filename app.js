148f4e574bea581b7af45edf32b0fc69a71dd838
Bryson Palmer <brysonpalmer@gmail.com>
2020-12-17 15:04:05 -0800

    Broke up the name property into first and last name components. UPdated all the files that handled the name property.

--- app.js
+++ app.js
@@ -34,10 +34,19 @@ function askUserForManagerInfo() {
 
         {
             type: "input",
-            name: "managerName",
-            message: "Enter your name: ",
-            validate: function validateName( managerName ) {
-                return managerName !== "";
+            name: "firstName",
+            message: "Enter your first name: ",
+            validate: function validateName( firstName ) {
+                return firstName !== "";
+            }
+        },
+
+        {
+            type: "input",
+            name: "lastName",
+            message: "Enter your last name: ",
+            validate: function validateName( lastName ) {
+                return lastName !== "";
             }
         },
 
@@ -71,7 +80,7 @@ function askUserForManagerInfo() {
     ]).then( (managerData) => {
 
         // Create new instance of a Manager
-        const newManager = new Manager ( managerData.managerName, managerData.idNumber, managerData.email, managerData.officeNumber );
+        const newManager = new Manager ( managerData.firstName, managerData.lastName, managerData.idNumber, managerData.email, managerData.officeNumber );
 
         teamMembers.push( newManager );
         
@@ -129,10 +138,19 @@ function askUserForEngineerInfo() {
 
         {
             type: "input",
-            name: "engineerName",
-            message: "Enter Engineer's name: ",
-            validate: function validateName( engineerName ) {
-                return engineerName !== "";
+            name: "firstName",
+            message: "Enter your first name: ",
+            validate: function validateName( firstName ) {
+                return firstName !== "";
+            }
+        },
+
+        {
+            type: "input",
+            name: "lastName",
+            message: "Enter your last name: ",
+            validate: function validateName( lastName ) {
+                return lastName !== "";
             }
         },
 
@@ -166,7 +184,7 @@ function askUserForEngineerInfo() {
     ]).then( (EngineerData) => {
 
         // Create new instance of a Manager
-        const newEngineer = new Engineer ( EngineerData.engineerName, EngineerData.idNumber, EngineerData.email, EngineerData.github );
+        const newEngineer = new Engineer ( EngineerData.firstName, EngineerData.lastName, EngineerData.idNumber, EngineerData.email, EngineerData.github );
 
         teamMembers.push ( newEngineer );
 
@@ -187,10 +205,19 @@ function askUserForInternInfo() {
 
         {
             type: "input",
-            name: "internName",
-            message: "Enter Intern's name: ",
-            validate: function validateName( internName ) {
-                return internName !== "";
+            name: "firstName",
+            message: "Enter your first name: ",
+            validate: function validateName( firstName ) {
+                return firstName !== "";
+            }
+        },
+
+        {
+            type: "input",
+            name: "lastName",
+            message: "Enter your last name: ",
+            validate: function validateName( lastName ) {
+                return lastName !== "";
             }
         },
 
@@ -224,7 +251,7 @@ function askUserForInternInfo() {
     ]).then( (InternData) => {
 
         // Create new instance of a Manager
-        const newIntern = new Intern ( InternData.internName, InternData.idNumber, InternData.email, InternData.school );
+        const newIntern = new Intern ( InternData.firstName, InternData.lastName, InternData.idNumber, InternData.email, InternData.school );
 
         teamMembers.push ( newIntern );
 
@@ -239,9 +266,9 @@ function createHtmlFile() {
 
     console.log( chalk.blueBright("\n\n------------------------------------------------\n") );
     console.log( chalk.greenBright("------------------ That's it! ------------------\n") );
-    console.log( chalk.greenBright("             *          *          *               ") );
-    console.log( chalk.greenBright("            * *        * *        * *              ") );
-    console.log( chalk.greenBright("           ** **      ** **      ** **             ") );
+    console.log( chalk.greenBright("            *          *          *               ") );
+    console.log( chalk.greenBright("           * *        * *        * *              ") );
+    console.log( chalk.greenBright("          ** **      ** **      ** **             ") );
     console.log( chalk.greenBright("\n-------- You have successfuly used the ---------\n") );
     console.log( chalk.greenBright("------ Software Engineer Team Generator. -------\n") );
     console.log( chalk.greenBright(" (HTML is in the output folder in the explorer) \n") );
@@ -256,32 +283,3 @@ function createHtmlFile() {
 };
 
 askUserForManagerInfo();
-
-// Make it work then make it modular
-
-// Add console.log("\n"); line breaks
-
-// Add color headers. Dif color for each employee type. Use chalk
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- test/Employee.test.js
+++ test/Employee.test.js
@@ -5,37 +5,62 @@ test("Can instantiate Employee instance", () => {
   expect(typeof(e)).toBe("object");
 });
 
-test("Can set name via constructor arguments", () => {
+test("Can set the first name via constructor arguments", () => {
   // Arrange
-  const name = "Alice";
+  const firstName = "Alice";
 
   // Act
-  const e = new Employee(name);
+  const e = new Employee(firstName);
 
   // Assert
-  expect(e.name).toBe(name);
+  expect(e.firstName).toBe(firstName);
+
+});
+
+test("Can set the last name via constructor arguments", () => {
+  // Arrange
+  const lastName = "Wonderland";
+
+  // Act
+  const e = new Employee(lastName);
+
+  // Assert
+  expect(e.lastName).toBe(lastName);
 
 });
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
+  const e = new Employee("Foo", "Bar", testValue);
   expect(e.id).toBe(testValue);
 });
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
+  const e = new Employee("Foo", "Bar", 1, testValue);
   expect(e.email).toBe(testValue);
 });
 
-test("Can get name via getName()", () => {
+test("Can get first name via getFirstName()", () => {
   // Arrange
   const testValue = "Alice";
   const e = new Employee(testValue);
 
   // Act
-  const result = e.getName();
+  const result = e.getFirstName();
+  
+  // Assert
+  expect( result ).toBe(testValue);
+  
+});
+
+test("Can get last name via getLastName()", () => {
+  // Arrange
+  const testValue = "Wonderland";
+  const e = new Employee(testValue);
+
+  // Act
+  const result = e.getLastName();
   
   // Assert
   expect( result ).toBe(testValue);
@@ -44,18 +69,18 @@ test("Can get name via getName()", () => {
 
 test("Can get id via getId()", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
+  const e = new Employee("Foo", "Bar", testValue);
   expect(e.getId()).toBe(testValue);
 });
 
 test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
+  const e = new Employee("Foo", "Bar", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
 
 test("getRole() should return \"Employee\"", () => {
   const testValue = "Employee";
-  const e = new Employee("Alice", 1, "test@test.com");
+  const e = new Employee("Alice", "Wonderland", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
 });
